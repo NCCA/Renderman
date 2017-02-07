a2002e3a7524ad8eb8754ef955a65cd64ea7f08f
jmacey <jmacey@bournemouth.ac.uk>
2017-02-07 19:02:12 +0000

    refactoring code to make it easier to change, added a common dir with the teapot drawing functions etc

--- RIS/SEEXPR/pattern.py
+++ RIS/SEEXPR/pattern.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python
 import prman
-
-def drawTeapot(ri,x=0,y=0,z=0) :
-  ri.TransformBegin()
-  ri.Translate(x,y,z)
-  ri.Rotate(45,0,1,0)
-  ri.Rotate( -90, 1 ,0 ,0)
-  ri.Scale( 0.2, 0.2, 0.2) 
-  ri.Geometry( "teapot")
-  ri.TransformEnd()  
-
-
+from ..common.functions import drawTeapot
 
 ri = prman.Ri() # create an instance of the RenderMan interface
 
---
+++ RIS/common/functions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import prman
+
+def drawTeapot(ri,x=0,y=0,z=0) :
+  ri.TransformBegin()
+  ri.Translate(x,y,z)
+  ri.Rotate(45,0,1,0)
+  ri.Rotate( -90, 1 ,0 ,0)
+  ri.Scale( 0.2, 0.2, 0.2) 
+  ri.Geometry( "teapot")
+  ri.TransformEnd()  

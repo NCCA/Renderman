7ecbb08ae016cc44e40179de6d6d4e445c00dc91
jmacey <jmacey@bournemouth.ac.uk>
2017-02-07 14:36:55 +0000

    converting Ian's demos to python

---
+++ RIS/disney.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import prman
+
+ri = prman.Ri() # create an instance of the RenderMan interface
+
+filename = "__render" 
+# this is the begining of the rib archive generation we can only
+# make RI calls after this function else we get a core dump
+ri.Begin('__render')
+
+# now we add the display element using the usual elements
+# FILENAME DISPLAY Type Output format
+ri.Display("simple.exr", "it", "rgba")
+ri.Format(1024,720,1)
+
+# setup the raytrace / integrators
+ri.Hider("raytrace" ,{"int incremental" :[1]})
+ri.PixelVariance (0.01)
+
+ri.Integrator ("PxrDefault" , "integrator")
+ri.Integrator ("PxrVCM" ,"integrator")
+ri.Integrator ("PxrDirectlighting" ,"integrator")
+ri.Integrator ("PxrPathTracer" ,"integrator")
+
+# now set the projection to perspective
+ri.Projection(ri.PERSPECTIVE,{ri.FOV:30}) 
+# Simple translate for our camera
+ri.Translate(0,0,5)
+ri.Rotate(-20,1,0,0)
+
+
+# now we start our world
+ri.WorldBegin()
+#Lighting We need geo to emit light
+ri.AttributeBegin()
+ri.Declare("areaLight" ,"string")
+ri.AreaLightSource( "PxrStdAreaLight", {ri.HANDLEID:"areaLight", "float exposure"  : [6]})
+ri.Translate( -2, 2 , 0)
+ri.Sphere( 0.3, -0.3, 0.3, 360)
+ri.AttributeEnd()
+
+# first teapot
+ri.TransformBegin()
+ri.Translate( -1, 0, 0)
+ri.Rotate(45,0,1,0)
+ri.Rotate( -90, 1 ,0 ,0)
+ri.Scale( 0.2, 0.2, 0.2)
+ri.Bxdf( "PxrDisney","bxdf", { 
+                        "color baseColor" : [ 0.8, 0.2, 0.2], 
+                        "float roughness" : [ 0.5 ], 
+                        "float clearcoat" : [ 0.2 ], 
+                        "float clearcoatGloss" : [1]
+                        })
+ri.Geometry( "teapot")
+ri.TransformEnd()
+
+# second teapot
+ri.TransformBegin()
+ri.Translate( 0, 0, 0)
+ri.Rotate(45,0,1,0)
+ri.Rotate( -90, 1 ,0 ,0)
+ri.Scale( 0.2, 0.2, 0.2)
+ri.Bxdf( "PxrDisney","bxdf", { 
+                        "color baseColor" : [ 0.8, 0.2, 0.2], 
+                        "float roughness" : [ 0.5 ], 
+                        "float clearcoat" : [ 0.5 ], 
+                        "float clearcoatGloss" : [1]
+                        })
+ri.Geometry( "teapot")
+ri.TransformEnd()
+
+# third teapot
+ri.TransformBegin()
+ri.Translate( 1, 0, 0)
+ri.Rotate(45,0,1,0)
+ri.Rotate( -90, 1 ,0 ,0)
+ri.Scale( 0.2, 0.2, 0.2)
+ri.Bxdf( "PxrDisney","bxdf", { 
+                        "color baseColor" : [ 0.8, 0.2, 0.2], 
+                        "float roughness" : [ 0.5 ], 
+                        "float clearcoat" : [ 1.0 ], 
+                        "float clearcoatGloss" : [1]
+                        })
+ri.Geometry( "teapot")
+ri.TransformEnd()
+
+# floor
+ri.TransformBegin()
+ri.Bxdf( "PxrDisney","bxdf", { 
+                        "color baseColor" : [ 1.0,1.0,1.0]
+                        })
+s=2.0
+face=[-s,0,-s, s,0,-s,-s,0,s, s,0,s]
+ri.Patch("bilinear",{'P':face})
+
+ri.TransformEnd()
+
+
+
+# end our world
+ri.WorldEnd()
+# and finally end the rib file
+ri.End()

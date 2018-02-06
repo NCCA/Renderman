4401447f1979177b4351b719a99bbb94bee24b62
jmacey <jmacey@bournemouth.ac.uk>
2018-02-06 20:27:14 +0000

    updated slides

--- Lecture1Intro/helloworld.py
+++ Lecture1Intro/helloworld.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # for bash we need to add the following to our .bashrc
 # export PYTHONPATH=$PYTHONPATH:$RMANTREE/bin
-import getpass
-import time
+
 # import the python renderman library
 import prman
 
@@ -15,9 +14,6 @@ ri.Begin('__render')
 # ArchiveRecord is used to add elements to the rib stream in this case comments
 # note the function is overloaded so we can concatinate output
 ri.ArchiveRecord(ri.COMMENT, 'Comments start with a #')
-ri.ArchiveRecord(ri.COMMENT, 'File HelloWorld.rib #')
-ri.ArchiveRecord(ri.COMMENT, "Created by " + getpass.getuser())
-ri.ArchiveRecord(ri.COMMENT, "Creation Date: " +time.ctime(time.time()))
 
 # now we add the display element using the usual elements
 # FILENAME DISPLAY Type Output format
--- slides/lecture1/PITCHME.md
+++ slides/lecture1/PITCHME.md
@@ -36,7 +36,7 @@
 <img src="slides/lecture1/images/pipeline.png" width="50%">
 
 
----?code=Lecture1Intro/HelloWorld.rib&lang=golang&title=Source: Golang File
+---?code=Lecture1Intro/HelloWorld.rib&lang=C&title=HelloWorld.rib
 
 @[1-13](A rib file is a simple text file, using an editor, type in the following)
 @[1-2](# used for comments)
@@ -48,5 +48,7 @@
 @[8,9](Translate back 2 in the Z)
 @[10,11](Draw a Sphere)
 
+
+---?code=Lecture1Intro/helloworld.py&lang=python&title=helloworld.py
 
 

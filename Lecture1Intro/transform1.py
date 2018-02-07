c1f1e1fc2a3ea325eeaf1421ce53421be67a4abe
jmacey <jmacey@bournemouth.ac.uk>
2018-02-07 10:28:35 +0000

    updated slides

--- Lecture1Intro/transform1.py
+++ Lecture1Intro/transform1.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python
-# for bash we need to add the following to our .bashrc
-# export PYTHONPATH=$PYTHONPATH:$RMANTREE/bin   
-import getpass
-import time
 # import the python renderman library
 import prman
 
@@ -12,15 +8,10 @@ filename = "transform1.rib"
 # this is the begining of the rib archive generation we can only
 # make RI calls after this function else we get a core dump
 ri.Begin(filename)
-# ArchiveRecord is used to add elements to the rib stream in this case comments
-# note the function is overloaded so we can concatinate output
-ri.ArchiveRecord(ri.COMMENT, 'File ' +filename)
-ri.ArchiveRecord(ri.COMMENT, "Created by " + getpass.getuser())
-ri.ArchiveRecord(ri.COMMENT, "Creation Date: " +time.ctime(time.time()))
 
 # now we add the display element using the usual elements
 # FILENAME DISPLAY Type Output format
-ri.Display("transform1.exr", "framebuffer", "rgba")
+ri.Display("transform1.exr", "it", "rgba")
 # Specify PAL resolution 1:1 pixel Aspect ratio
 ri.Format(720,576,1)
 # now set the projection to perspective

5e3081c35d7d727d5f00ff2a6f39e373b473cde9
jmacey <jmacey@bournemouth.ac.uk>
2018-02-19 14:52:29 +0000

    added a colour shader to intro so we can use Cs param lists

--- Lecture1Intro/Param.py
+++ Lecture1Intro/Param.py
@@ -3,16 +3,16 @@
 import prman
 
 ri = prman.Ri() # create an instance of the RenderMan interface
-ri.Option("rib", {"string asciistyle": "indented"})
+ri.Option('rib', {'string asciistyle': 'indented'})
 
-filename = "Param.rib"
+filename = 'Param.rib'
 # this is the begining of the rib archive generation we can only
 # make RI calls after this function else we get a core dump
 ri.Begin('__render')
 
 # now we add the display element using the usual elements
 # FILENAME DISPLAY Type Output format
-ri.Display("Param.exr", "it", "rgba")
+ri.Display('Param.exr', 'it', 'rgba')
 # Specify PAL resolution 1:1 pixel Aspect ratio
 ri.Format(720,575,1)
 # now set the projection to perspective
@@ -20,15 +20,16 @@ ri.Projection(ri.PERSPECTIVE,{ri.FOV:50})
 
 # now we start our world
 ri.WorldBegin()
-ri.Bxdf( "PxrDiffuse","bxdf", 
+ri.Pattern( 'colour', 'colourShader')
+ri.Bxdf( 'PxrDiffuse','bxdf', 
 {
-"color diffuseColor" : [ 1.0, 0.0, 0.0]
+  'reference color diffuseColor' : ['colourShader:Cout']
 })
 ri.Translate(0,0,3)
 ri.TransformBegin()
 colours=[1,0,0,0,0,1,1,0,0,0,1,0]
 ri.Rotate(90,1,1,1)
-ri.Sphere(1,-1,1,360,{"Cs":colours})
+ri.Sphere(1,-1,1,360,{'Cs':colours})
 ri.TransformEnd()
 ri.WorldEnd()
 # and finally end the rib file

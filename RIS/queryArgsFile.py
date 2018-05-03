1e69687cdb8380a4aa8ff3d93a32fc5c739ef191
jmacey <jmacey@bournemouth.ac.uk>
2018-05-03 14:01:59 +0100

    working queryArgs script

--- RIS/queryArgsFile.py
+++ RIS/queryArgsFile.py
@@ -13,11 +13,22 @@ def processArgFile(file,output) :
   shaderType=''.join(shaderType[0].upper()+shaderType[1:]) # First letter is capital bxdf to Bxdf
   # May also have Filter types such as LightFilter (lightFilter)
   shaderType=shaderType.replace('filter','Filter')
+  strings=''
     
   if output == 'rib' :
-    print "rib"
+    strings+=( '%s "%s" "id" \n' %(shaderType,pluginName))
+    for t in e.findall('param'):
+      dataType=t.get('type')
+      name=t.get('name')
+      defaultValue=t.get('default')
+      if defaultValue == None :
+        defaultValue="'No Value'" # sometimes there is no default
+      elif dataType=='string' :
+        defaultValue="'"+defaultValue+"'" # strings need to be quoted
+      elif dataType == 'float' : 
+        defaultValue=defaultValue.replace('f','') # seems some floats us 0.0f
+      strings+='\t"%s %s"  [%s] \n' %(dataType,name,defaultValue)
   elif output=='python' :
-    strings=''
     strings+=( "ri.%s('%s','id',\n" %(shaderType,pluginName))
     strings+='{\n'
     for t in e.findall('param'):
@@ -33,25 +44,44 @@ def processArgFile(file,output) :
         if dataType == 'float' : 
           defaultValue=defaultValue.replace('f','') # seems some floats us 0.0f
       strings+="\t'%s %s' : [%s], \n" %(dataType,name,defaultValue)
-      #print type(t[1])
     strings+='})\n'
-    print strings
-
   else :
-    print "text"
-  
-
-def main(directory,output) :
-  if os.path.isdir(directory) :
-    files=os.listdir(directory)
-    for file in files :
-      if file.endswith('.args') :
-        file='%s/%s' %(directory,file)
-        processArgFile(file,output)
-        print '#' * 40
-  elif os.path.isfile(directory) :
-        processArgFile(directory,output)
     
+    strings+=( '%s %s\n' %(shaderType,pluginName))
+    for t in e.findall('param'):
+      dataType=t.get('type')
+      name=t.get('name')
+      defaultValue=t.get('default')
+      strings+=('%s %s %s \n' %(dataType,name,defaultValue))
+  return strings
+
+def main(directory,output,outputFile) :
+
+  if outputFile !='' :
+    with open(outputFile,'w') as outFile :
+      if os.path.isdir(directory) :
+        files=os.listdir(directory)
+        for file in files :
+          if file.endswith('.args') :
+            file='%s/%s' %(directory,file)
+            data=processArgFile(file,output)
+            if outFile is not None :
+              outFile.write(data)
+              outFile.write('#' * 40+'\n')
+      elif os.path.isfile(directory) :
+            data=processArgFile(directory,output)
+            outFile.write(data)
+  else :
+      if os.path.isdir(directory) :
+        files=os.listdir(directory)
+        for file in files :
+          if file.endswith('.args') :
+            file='%s/%s' %(directory,file)
+            print processArgFile(file,output)
+            print '#' * 40
+      elif os.path.isfile(directory) :
+        print processArgFile(directory,output)
+  
 
 
 if __name__ == '__main__':
@@ -60,6 +90,9 @@ if __name__ == '__main__':
   parser.add_argument('directory',help='directory to use can be explicit single file',action='store',default='.')
   parser.add_argument('--rib', '-r' , action='count',help='render to rib not framebuffer')
   parser.add_argument('--py', '-p' , action='count',help='render to rib not framebuffer')
+  parser.add_argument('--file', '-f' ,nargs='?',
+                      const='', default='',type=str,
+                      help='dump output to file not stdout')
 
   output=''
   args = parser.parse_args()
@@ -67,6 +100,10 @@ if __name__ == '__main__':
     output='rib'
   if args.py :
     output='python'
-  main(args.directory,output)
+  outputFile=''
+  if args.file != '' :
+    outputFile=args.file
+
+  main(args.directory,output,outputFile)
   
 

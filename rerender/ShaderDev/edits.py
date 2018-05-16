04b7a4722ad276df6fd78b284648d68e867676c6
jmacey <jmacey@bournemouth.ac.uk>
2018-05-16 22:53:57 +0100

    working well

--- rerender/ShaderDev/edits.py
+++ rerender/ShaderDev/edits.py
@@ -1,16 +1,26 @@
 ## change shader binding
-"""
-ri.EditBegin('option')
-print "moving Camera"
-# Simple translate for our camera
-ri.Projection(ri.PERSPECTIVE,{ri.FOV:[40]})
-cam=Camera(Vec4(1,1.4,5.8),Vec4(0,0,0),Vec4(0,1,0))
-cam.place(ri)
-ri.Camera( "world")
+# Normal Camera
+cam='Default'
+if cam == 'Normal' :
+    ri.EditBegin('option')
+    print "moving Camera"
+    # Simple translate for our camera
+    ri.Projection(ri.PERSPECTIVE,{ri.FOV:[40]})
+    cam=Camera(Vec4(0,1.4,2.8),Vec4(0,0,0),Vec4(0,1,0))
+    cam.place(ri)
+    ri.Camera( "world")
+    ri.EditEnd() 
+elif cam == 'Top' :
+#Top Camera
+    ri.EditBegin('option')
+    print "moving Camera"
+    # Simple translate for our camera
+    ri.Projection(ri.PERSPECTIVE,{ri.FOV:[50]})
+    cam=Camera(Vec4(0,2.4,0),Vec4(0,0,0),Vec4(0,0,1))
+    cam.place(ri)
+    ri.Camera( "world")
+    ri.EditEnd() 
 
-ri.EditEnd() 
-
-"""
 editName='sceneObject'
 ri.EditBegin( 'attribute',
 {
@@ -22,23 +32,14 @@ ri.Pattern('PxrVoronoise','voronoise2',
 {
 	'int surfacePosition' : [0], 
 	'float frequency' : [22.0], 
-	'int octaves' : [20], 
-	'float gain' : [0.5],   
-	'float lacunarity' : [12.0], 
+	'int octaves' : [2], 
+	'float gain' : [2],   
+	'float lacunarity' : [9.0], 
 	'float jitter' : [.2], 
 	'float smoothness' : [1.0], 
 	'int turbulent' : [1],
 
 })
-#ri.EditEnd() 
-
-## bxdf parameter edit for the floor
-#editName='sceneObject'
-print 'change all of the pxrsurface instances' 
-# ri.EditBegin( 'attribute',
-# {
-#   'string scopename'  : [editName]
-# }) 
 
 
 ri.Bxdf('PxrSurface', 'plastic',

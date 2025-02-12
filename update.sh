41f031b284b465367600dc0854683c32b052dc73
Jon Macey <jmacey@bournemouth.ac.uk>
2025-02-12 11:08:53 +0000

    updated the update script for rmanpy3 not rmanpy

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ find "$SEARCH_DIR" -type f -name "*.py" | while read -r file; do
     # Check if the first line contains the Python shebang
     if head -n 1 "$file" | grep -q "#!/usr/bin/env python"; then
         # Use sed to replace the line
-        sed -i '1s|#!/usr/bin/env python|#!/usr/bin/env rmanpy|' "$file"
+        sed -i '1s|#!/usr/bin/env python|#!/usr/bin/env rmanpy3|' "$file"
         echo "Updated: $file"
     fi
 done
